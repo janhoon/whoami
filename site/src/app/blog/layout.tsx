f3273af8c73a20962ecb5e7e8303399683814363
Jan Hoon <janhoon97@gmail.com>
2025-01-27 15:05:24 +0000

    feat: added navigation for blogs back to the blogs landing

--- site/src/app/blog/layout.tsx
+++ site/src/app/blog/layout.tsx
@@ -9,10 +9,11 @@ export default function BlogLayout({
 	return (
 		<div className="flex flex-col items-center justify-center w-full">
 			<div className="flex flex-col items-center justify-center w-2/3">
-				<div className="flex flex-row items-end justify-start gap-2 w-full py-4">
-					<span className="text-4xl font-bold">Jan Hoon</span>
-					<span className="text-2xl">blog</span>
-                    <div className="grow" />
+				<div className="flex flex-row items-center justify-between w-full py-4">
+					<Link href="/blog" className="flex flex-row items-end gap-2">
+						<span className="text-4xl font-bold">Jan Hoon</span>
+						<span className="text-2xl">blog</span>
+					</Link>
 					<Link href="/" className="text-sm text-green-600">
 						<HomeIcon className="w-6 h-6" />
 					</Link>
