01df74c799b940d11478f40914cfa82cdce8fc31
Jan Hoon <janhoon97@gmail.com>
2025-02-11 16:52:31 +0000

    fix: not found page formatting

--- site/src/app/not-found.tsx
+++ site/src/app/not-found.tsx
@@ -1,11 +1,18 @@
-import Link from 'next/link'
- 
+import { Home } from "lucide-react";
+import Link from "next/link";
+
 export default function NotFound() {
-  return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
-  )
+	return (
+		<div className="w-screen h-screen flex flex-col items-center justify-center">
+			<h2 className="text-2xl font-bold">Not Found</h2>
+			<p className="text-gray-500">Could not find requested resource</p>
+			<Link
+				href="/"
+				className="flex items-center gap-2 pt-4 text-green-400 hover:text-green-500"
+			>
+				<Home />
+				Return Home
+			</Link>
+		</div>
+	);
 }
