be0e5c75b9955d5f88c63d3162871c4d47b7f6c3
Jan Hoon <janhoon97@gmail.com>
2025-02-12 22:01:00 +0000

    blog: cleanup and short completeness

--- site/src/app/blog/cadac/page.tsx
+++ site/src/app/blog/cadac/page.tsx
@@ -56,92 +56,62 @@ export default function Cadac() {
         aiming for.
       </p>
       <p>
-        So how do we ensure completeness? I have 5 key areas of focus
-        for completeness:
+        So how do we ensure completeness? I have 2 key areas of focus for
+        completeness:
       </p>
-      <BlogH4>1. Raw Source Data</BlogH4>
+      <BlogH4>1. Business and Application Data</BlogH4>
       <p>
-        Raw source data should always be the starting point. Insights and value
-        will not necessarily only come from the places you expect to find them,
-        thus you always keep an eye on your source data, both reviewing the data
-        you already have as well as finding new sources to grow you foundation.
-        So then, what does raw source data even mean? Well, anything, but
-        let&apos;s go into some examples:
+        This should be the starting point. Insights and value will not
+        necessarily only come from the places you expect to find them, thus you
+        always keep an eye on your own data, both reviewing the data you already
+        have as well as finding new sources to grow your foundation. But what do
+        I mean by business and application data? Let&apos;s go through some
+        examples:
       </p>
       <ul className="flex flex-col list-disc list-outside pt-2 pl-8 gap-2">
         <li>
-          <span className="font-bold">Databases:</span> This seems like a
-          no-brainer, but don&apos;t just think of databases within you business
-          as &quot;another source&quot;, get more involved in the teams
-          maintaining the database and guide them with potential improvements.
-          Remember, with many datasources your business is not the owner and
-          will rely on others to provide the data for you but if you have the
-          opportunity to get more value from the sources you use, you should do
-          it. Simple example: you have a table in a database containing customer
-          phone numbers as a string. Why not ask the team to rather split the
-          phone number into country code and number when they request this data
-          from the user? Suddenly you you now not only have customer phone
-          numbers for operation, but you are able have a proxy to figure out in
-          which countries you have more customers.
+          <span className="font-bold">Databases:</span> Your services and
+          applications will generate data as they are used, don&apos;t just
+          think use tables you think are useful, bring everything even if you
+          don&apos;t need all of it at first. Also revise and update your
+          databases to match your needs. If there is something you can track and
+          store to follow and experiment or new feature, do it and store it.
         </li>
         <li>
-          <span className="font-bold">API:</span> Firstly, no, this does not
-          mean you should integrate the database behind your APIs. APIs
-          generally contain tonnes of valuable data, if you set them up
-          correctly. When building APIs you should always think about what data
-          you could track regarding the API usage and not just the data it
-          generates via interactions with your users. Rather think of metrics
-          such as RPS (requests per seconds) or rate limits imposed on clients.
-          These can provide powerful information regarding how and when your
-          users are using your services and could teach you a lot about what is
-          the best times to reach out to them, or maybe you have a larger
-          customer hitting your rate limits and you can now offer them a better
-          deal to provide a better, less interrupted experience.
+          <span className="font-bold">API Metrics:</span> If your business
+          exposes APIs to customers or other services, you are likely already
+          recording metrics to track issues and performance. These metrics can
+          often tell you a lot about how your customers are using your product
+          and what they are interested in.
         </li>
         <li>
-          <span className="font-bold">Logs:</span> This is a bit more tricky and
-          depending on your industry could require a bit more effort, but logs
-          are incredibly powerful. Engineering and ops teams will often use
-          logs to find issues within system, but as the data person you have a
-          unique opportunity to use these logs to find insights. Think of logs
-          as a &quot;semi&quot; structured list of events you customers perform.
-          If you can figure out from a list of log lines what lines are the same
-          users, you can start picking up on usage patterns and more commonly
-          viewed features, without the users ever having to input any new data.
-          If a GET request is made, we usually don&apos;t get this information
-          from a database.
+          <span className="font-bold">Logs:</span> These are commonly used by
+          developers to find issues and bugs in services, but they can also be
+          used to track user behavior and other patterns in how your services
+          are used.
         </li>
         <li>
-          <span className="font-bold">External Data Providers:</span> Your own
-          services are not the only source of data. You should always be looking
-          for more sources of data within your business and industry to ensure
-          you a) have a complete picture of your business and b) have a complete
-          picture of your industry. Inside your business you might be using a
-          CRM and Google Ads to track your existing and potential customers. You
-          should have this data in your data platform to do comparisons with
-          other sources such as the performance of a new feature or product
-          campaign. As for your industry, you should should keep an eye on
-          trends of competitors and industry leaders to find more growth
-          opportunities in your own business.
+          <span className="font-bold">External Data Providers:</span> Find data
+          providers that can give you insights into your customers, your market
+          or competitors. This can be used to both enrich and benchmark your own
+          business as you grow
         </li>
       </ul>
       <p>
-        Be creative and make sure you have the data necessary to make the right
-        decisions.
+        Be creative and make sure you are generating the data necessary to make
+        the right decisions.
       </p>
       <BlogH4>2. ETL, ELT or whatever you want to call it</BlogH4>
       <p>
-        You&apos;ve selected your sources and have setup some data integrations
-        service to pull all of the data into a central location, but the job is
-        not done. To build trust on the completeness of you data, you need to
-        not just cover a wide range of data, but cover a wide range, without
-        losing any data in the moving process.
+        You&apos;ve selected your sources and have setup some data integration
+        service to pull all of the data into a central location, be it
+        Snowflake, Databricks or Excel, but the job is not done. To build trust
+        on the completeness of you data, you need to not just cover a wide range
+        of data, but without losing any data in the moving process.
       </p>
-      <BlogH4>3. User Generated Data</BlogH4>
       <p>
-        User generated data is data that is created by users of your product.
-        This could be data such as user feedback, user actions, user
-        interactions or any other data that is created by users.
+        One way to improve the trust in your data completeness is to run period
+        checks comparing the source data with the data in you data warehouse.
       </p>
       <BlogFooter
         author="Jan Hoon"
