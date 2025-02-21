05901bed8c4cd63415d234f6bf011f21ddb92583
Jan Hoon <janhoon97@gmail.com>
2025-02-21 14:38:06 +0000

    feat: proxy posthog requests through next

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://eu.i.posthog.com/decide",
+      },
+    ];
+  },
+  skipTrailingSlashRedirects: true,
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
--- site/src/components/PosthogProvider.tsx
+++ site/src/components/PosthogProvider.tsx
@@ -8,7 +8,7 @@ import PostHogPageView from "./PostHogPageView";
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      api_host: '/ingest',
       person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     });
